2b190ffbbc13010752a93062eda018916f77da1c
MatthiasHazeleger <matthiashaze@gmail.com>
2017-09-06 15:37:25 +0200

    Delete the API function for now.

--- helper.php
+++ helper.php
@@ -4,10 +4,6 @@ class ModDashboardHelper
      
     public static function getSites()
     {
-      define('API_KEY', 'df071b1cfaf900008702d26f2998d0bf');
-      define('BASE_URL', 'https://app.watchful.li/api/v1');
-      $ch = curl_init(BASE_URL . '/sites');
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Api-Key: ' . API_KEY));
-      return curl_exec($ch);
+      return 'Hello!';
     }
 }
