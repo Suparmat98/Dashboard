527caf85b5941838ab3d6b8d1a9f922aa8ebcfe3
MatthiasHazeleger <matthiashaze@gmail.com>
2017-09-06 15:52:18 +0200

    Add a non-ssl stripped version of the last getSites function.

--- helper.php
+++ helper.php
@@ -4,6 +4,9 @@ class ModDashboardHelper
      
     public static function getSites()
     {
-      return 'Hello!';
+      $ch = curl_init('https://app.watchful.li/api/v1' . '/sites');
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Api-Key: ' . 'df071b1cfaf900008702d26f2998d0bf'));
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      return curl_exec($ch);
     }
 }
